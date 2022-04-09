92cc20ab87cf798fbecba60c30d20200c98256e2
cat-friend <denisepli@gmail.com>
2022-04-09 19:45:26 -0400

    need to fix logic, result corresponds to home vs away team, not to position within nested array

---
+++ algoexpert/easy/tournament-winner.js
@@ -0,0 +1,18 @@
+// iterate through results, compare value at winner
+
+function tournamentWinner(competitions, results) {
+    const scoreTotals = {};
+    let teamThatGetsPoints = competitions[0][results[0]];
+    scoreTotals[teamThatGetsPoints] = 3
+    let winner = teamThatGetsPoints;
+    for (let i = 1; i < results.length; i++) {
+        if (scoreTotals.hasOwnProperty(teamThatGetsPoints)) {
+            scoreTotals[teamThatGetsPoints] += 3;
+        }
+        else { scoreTotals[teamThatGetsPoints] = 3 };
+        if (scoreTotals[winner] < scoreTotals[teamThatGetsPoints]) {
+            winner = teamThatGetsPoints;
+        }
+    }
+    return winner;
+}
