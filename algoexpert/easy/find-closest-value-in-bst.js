0cb4ca5c89ac3ec4436a8d7bee70a26fabf9692f
cat-friend <denisepli@gmail.com>
2022-04-10 12:52:24 -0400

    some pseudo-code to get started

---
+++ algoexpert/easy/find-closest-value-in-bst.js
@@ -0,0 +1,8 @@
+// return closest value to that target value contained in the BST
+// breadth-first search - keep track of abs value of difference; if abs value of difference < current diff, then curr num is the closest number
+// proceed until all nodes touched
+// bfs is queue -> use push and shift
+
+function findClosestValueInBst(tree, target) {
+    
+  }
