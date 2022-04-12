d43526747274688b3d01e9f00f1c38f4040a8c0c
cat-friend <denisepli@gmail.com>
2022-04-12 09:30:57 -0400

    new problem:  class photos

---
+++ algoexpert/easy/class-photos.js
@@ -0,0 +1,7 @@
+function classPhotos(redShirtHeights, blueShirtHeights) {
+    // Write your code here.
+    return false;
+  }
+
+  // Do not edit the line below.
+  exports.classPhotos = classPhotos;
