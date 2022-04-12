e0e4c164e86711dab885dfafef17376a08ae83ff
cat-friend <denisepli@gmail.com>
2022-04-12 10:17:45 -0400

    new problem: remove dupes from LL

---
+++ algoexpert/easy/remove-dupes-from-linked-list.js
@@ -0,0 +1,16 @@
+// This is an input class. Do not edit.
+class LinkedList {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  function removeDuplicatesFromLinkedList(linkedList) {
+    // Write your code here.
+    return null;
+  }
+
+  // Do not edit the lines below.
+  exports.LinkedList = LinkedList;
+  exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
