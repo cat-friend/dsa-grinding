ad79b6b27894c6b594d961d290060a518b3fecf0
cat-friend <denisepli@gmail.com>
2022-04-12 10:26:07 -0400

    completed on first try! wow ;_;

--- algoexpert/easy/remove-dupes-from-linked-list.js
+++ algoexpert/easy/remove-dupes-from-linked-list.js
@@ -6,9 +6,26 @@ class LinkedList {
     }
   }
 
+  // removing dupes -- make a set! if the set already has it, make node.next = node.next.next
+  // then proceed to the next node
+  // if the set doesn't already have it, currNode = node.next;
+  // `while` loop: need to only execute if there's a next one
   function removeDuplicatesFromLinkedList(linkedList) {
-    // Write your code here.
-    return null;
+    const existingNodes = new Set();
+    let currNode = linkedList;
+    existingNodes.add(currNode.value);
+    let nextNode;
+    while (currNode.next) {
+        nextNode = currNode.next;
+        if (existingNodes.has(nextNode.value)) {
+            currNode.next = currNode.next.next;
+        }
+        else {
+            existingNodes.add(nextNode.value);
+            currNode = nextNode;
+        }
+    }
+    return linkedList;
   }
 
   // Do not edit the lines below.
