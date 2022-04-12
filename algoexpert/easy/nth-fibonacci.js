5b8e7f63b4c60ad4785c80f7e8156c63e3f97c05
cat-friend <denisepli@gmail.com>
2022-04-12 10:43:34 -0400

    completed in one try!

--- algoexpert/easy/nth-fibonacci.js
+++ algoexpert/easy/nth-fibonacci.js
@@ -1,5 +1,14 @@
+// F0 = 0
+// F1 = 1
+// example fibSeq = [0, 1, 1, 2, 3, 5];
+// nth sequence is fibSeq[n - 1];
+
 function getNthFib(n) {
-    // Write your code here.
+    const fibSequence = [0, 1];
+    while (fibSequence.length < n) {
+        fibSequence.push(fibSequence[fibSequence.length - 2] + fibSequence[fibSequence.length - 1]);
+    }
+    return fibSequence[n - 1];
   }
 
   // Do not edit the line below.
