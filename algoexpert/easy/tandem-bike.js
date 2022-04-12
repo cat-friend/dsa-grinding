d7d5ef968041568eb056c34b93e8d8c1fada3964
cat-friend <denisepli@gmail.com>
2022-04-12 09:46:19 -0400

    new problem: tandem bicycle

---
+++ algoexpert/easy/tandem-bike.js
@@ -0,0 +1,7 @@
+function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
+    // Write your code here.
+    return 0;
+  }
+
+  // Do not edit the line below.
+  exports.tandemBicycle = tandemBicycle;
