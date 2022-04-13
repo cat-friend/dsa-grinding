e9c582c250156200147072780f2c5c3354a8a9c7
cat-friend <denisepli@gmail.com>
2022-04-13 11:06:05 -0400

    completed; needed to look at walkthrough

--- algoexpert/easy/binary-search.js
+++ algoexpert/easy/binary-search.js
@@ -1,6 +1,25 @@
-function binarySearch(array, target) {
-    // Write your code here.
-  }
+// binary search - look at middle -- if target > array[mid], splice from mid -> end
+// dont need to do recursion if you keep track of left and right
 
-  // Do not edit the line below.
-  exports.binarySearch = binarySearch;
+function binarySearch(array, target) {
+    return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(array, target, left, right) {
+    while (left <= right) {
+        const midpoint = Math.floor((left + right) / 2);
+        const eleMatch = array[midpoint];
+        if (eleMatch === target) {
+            return midpoint;
+        }
+        if (eleMatch >  target) {
+            right = midpoint - 1
+        }
+        else left = midpoint + 1
+    }
+    return -1;
+
+}
+
+// Do not edit the line below.
+exports.binarySearch = binarySearch;
