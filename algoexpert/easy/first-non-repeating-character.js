2e568a61247c2ca2937c25e3af15acde6e736248
cat-friend <denisepli@gmail.com>
2022-04-13 16:54:19 -0400

    optimal space/time complexity on first try!

--- algoexpert/easy/first-non-repeating-character.js
+++ algoexpert/easy/first-non-repeating-character.js
@@ -1,6 +1,21 @@
 function firstNonRepeatingCharacter(string) {
-
-    return -1;
+    const lettersInString = {};
+    for (const char of string) {
+        if (lettersInString.hasOwnProperty(char)) {
+            lettersInString[char]++;
+        }
+        else {
+            lettersInString[char];
+            lettersInString[char] = 1;
+        }
+    }
+    for (let i = 0; i < string.length; i++) {
+        let char = string[i];
+        if (lettersInString[char] === 1) {
+            return i;
+        }
+    }
+    return -1
   }
 
   // Do not edit the line below.
