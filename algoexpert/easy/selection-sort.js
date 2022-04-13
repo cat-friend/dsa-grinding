f0c26299296720a39baacc44ae9e2690a0ba8c02
cat-friend <denisepli@gmail.com>
2022-04-13 14:17:36 -0400

    completed!

--- algoexpert/easy/selection-sort.js
+++ algoexpert/easy/selection-sort.js
@@ -1,6 +1,20 @@
 function selectionSort(array) {
-    // Write your code here.
-  }
+    for (let i = 0; i < array.length; i++) {
+        let currMinIndex = i;
+        let minIndex = i;
+        let min = array[i];
+        while (minIndex < array.length) {
+            if (array[minIndex] < min) {
+                min = array[minIndex];
+                currMinIndex = minIndex;
+            }
+            minIndex++
+        }
+        array[currMinIndex] = array[i]
+        array[i] = min;
+    }
+    return array;
+}
 
-  // Do not edit the line below.
-  exports.selectionSort = selectionSort;
+// Do not edit the line below.
+exports.selectionSort = selectionSort;
