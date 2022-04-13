db11223f6ecdfb8a2d5a1af852cec919a9ce58fd
cat-friend <denisepli@gmail.com>
2022-04-13 16:27:11 -0400

    completed run-length-encoding

--- algoexpert/easy/run-length-encoding.js
+++ algoexpert/easy/run-length-encoding.js
@@ -1,6 +1,28 @@
 function runLengthEncoding(string) {
-    // Write your code here.
+  // Write your code here.
+  let counter = 1;
+  let referenceChar = string[0]
+  const newString = [];
+  for (let i = 1; i < string.length; i++) {
+    let currChar = string[i];
+    if (currChar === referenceChar) {
+      counter++
+    }
+    else {
+      if (counter) {
+        newString.push(`${counter}${referenceChar}`);
+      }
+      counter = 1;
+      referenceChar = currChar;
+    }
+    if (counter === 9) {
+      newString.push(`${counter}${referenceChar}`);
+      counter = 0;
+    }
   }
+  newString.push(`${counter}${referenceChar}`);
+  return newString.join("");
+}
 
-  // Do not edit the line below.
-  exports.runLengthEncoding = runLengthEncoding;
+// Do not edit the line below.
+exports.runLengthEncoding = runLengthEncoding;
