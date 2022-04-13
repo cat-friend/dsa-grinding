ac07d96621f9ab9387a8200fd968789088064087
cat-friend <denisepli@gmail.com>
2022-04-13 16:37:02 -0400

    completed generate-document; suboptimal solution

--- algoexpert/easy/generate-document.js
+++ algoexpert/easy/generate-document.js
@@ -1,7 +1,20 @@
 function generateDocument(characters, document) {
-    // Write your code here.
-    return false;
-  }
+    if (document == "") {
+        return true;
+    }
+    characters = [...characters]
+    for (let i = 0; i < document.length; i++) {
+        let currChar = document[i];
+        let currCharIndexInCharacters = characters.indexOf(currChar);
+        if (currCharIndexInCharacters > -1) {
+            characters.splice(currCharIndexInCharacters, 1);
+        }
+        else return false;
+    }
+    return true;
+}
 
-  // Do not edit the line below.
-  exports.generateDocument = generateDocument;
+// Do not edit the line below.
+exports.generateDocument = generateDocument;
+
+// try using a hash table
