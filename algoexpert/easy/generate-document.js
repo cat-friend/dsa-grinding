6f3fd3da601cc13e55a9c86f7959bcc12db165a2
cat-friend <denisepli@gmail.com>
2022-04-13 16:27:52 -0400

    new problem: generate document

---
+++ algoexpert/easy/generate-document.js
@@ -0,0 +1,7 @@
+function generateDocument(characters, document) {
+    // Write your code here.
+    return false;
+  }
+
+  // Do not edit the line below.
+  exports.generateDocument = generateDocument;
