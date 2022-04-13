76946da0d1c90fee06d5143e1e0da8b30891f722
cat-friend <denisepli@gmail.com>
2022-04-13 11:29:09 -0400

    insertion sort in progress - lunch break

--- algoexpert/easy/insertion-sort.js
+++ algoexpert/easy/insertion-sort.js
@@ -1,5 +1,13 @@
 function insertionSort(array) {
-    // Write your code here.
+    for (let i = 1; i < array.length; i++) {
+        const insertionIndex = 0;
+        let sortIndex = i - 0;
+        let currNum = array[i]
+        while (sortIndex >= 0) {
+            let prevNum = array[sortIndex];
+            if (currNum < prevNum)
+        }
+    }
   }
 
   // Do not edit the line below.
