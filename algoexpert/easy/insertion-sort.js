d97d20577303870114afabf81feaad186558c162
cat-friend <denisepli@gmail.com>
2022-04-13 12:55:48 -0400

    insertion-sort completed!

--- algoexpert/easy/insertion-sort.js
+++ algoexpert/easy/insertion-sort.js
@@ -1,14 +1,17 @@
 function insertionSort(array) {
     for (let i = 1; i < array.length; i++) {
-        const insertionIndex = 0;
-        let sortIndex = i - 0;
-        let currNum = array[i]
-        while (sortIndex >= 0) {
-            let prevNum = array[sortIndex];
-            if (currNum < prevNum)
+        let j = i;
+        while (j > 0) {
+            let currNum = array[j]
+            if (currNum < array[j-1]) {
+                array[j] = array[j - 1];
+                array[j - 1] = currNum
+            }
+            j--
         }
     }
-  }
+    return array;
+}
 
-  // Do not edit the line below.
-  exports.insertionSort = insertionSort;
+// Do not edit the line below.
+exports.insertionSort = insertionSort;
