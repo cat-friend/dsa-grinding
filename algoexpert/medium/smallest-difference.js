e67ee16965acedc41c52f62d706dd9c6d80055ff
cat-friend <denisepli@gmail.com>
2022-04-15 19:59:13 -0400

    working on figuring out the conditional logic

--- algoexpert/medium/smallest-difference.js
+++ algoexpert/medium/smallest-difference.js
@@ -1,6 +1,31 @@
 function smallestDifference(arrayOne, arrayTwo) {
-    // Write your code here.
-  }
+    arrayOne.sort((a, b) => a - b);
+    arrayTwo.sort((a, b) => a - b);
+    let possSmallestAbsDiff = Math.abs(arrayOne[0] - arrayTwo[0]);
+    let possNums = [arrayOne[0], arrayTwo[0]];
+    let indexArrayOne = 0;
+    let indexArrayTwo = 0;
+    for (let i = 0; i < arrayOne.length; i++) {
+        let eleArrayOne = arrayOne[indexArrayOne];
+        let eleArrayTwo = arrayTwo[indexArrayTwo];
+        while (indexArrayTwo < arrayTwo.length) {
+            let currAbsDiff = Math.abs(eleArrayOne - eleArrayTwo);
 
-  // Do not edit the line below.
-  exports.smallestDifference = smallestDifference;
+            if (currAbsDiff === 0) {
+                return [eleArrayOne, eleArrayTwo];
+            }
+            if (currAbsDiff < possSmallestAbsDiff) {
+                possNums = [eleArrayOne, eleArrayTwo];
+            }
+            if (eleArrayOne < eleArrayTwo) {
+                indexArrayOne++;
+            }
+            else indexArrayTwo++;
+        }
+
+    }
+    return possNums;
+}
+
+// Do not edit the line below.
+exports.smallestDifference = smallestDifference;
