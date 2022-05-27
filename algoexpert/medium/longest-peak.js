58c7c244411e802a97ac3168e5e2bb1147626e79
cat-friend <denisepli@gmail.com>
2022-05-26 19:40:30 -0700

    restarted longest-peak, added hints

--- algoexpert/medium/longest-peak.js
+++ algoexpert/medium/longest-peak.js
@@ -1,52 +1,9 @@
 function longestPeak(array) {
-    // peak = length of increasing + length of decreasing
-    if (!array.length) return 0;
-    let currPeakArray = [array[0]];
-    let hasPeaked = false;
-    let longestPeakArray = [];
-    // traverse array
+    /**
+     * hints:  find peak
+     * expand outwards until you no longer have a peak
+     */
+  }
 
-
-    for (let i = 1; i < array.length; i++) {
-        const currNum = array[i];
-        const prevNum = array[i - 1];
-        console.log("start")
-        console.log("hasPeaked?", hasPeaked);
-        console.log("currNum", currNum);
-        console.log("prevNum", prevNum);
-        console.log("currPeakArray", currPeakArray)
-        console.log("longestePeakArray", longestPeakArray)
-        if (hasPeaked) {
-            if (currNum < prevNum) currPeakArray.push(currNum)
-            if (currNum > prevNum || currNum === prevNum) {
-                if (currPeakArray.length > longestPeakArray.length) {
-                    longestPeakArray = currPeakArray;
-                    currPeakArray = [currNum];
-                }
-                if (currNum > prevNum) {
-                    currPeakArray = [prevNum, currNum]
-                }
-                hasPeaked = false;
-            }
-
-        }
-        else {
-            if (currNum > prevNum) currPeakArray.push(currNum);
-            if (currNum < prevNum) {
-                hasPeaked = true;
-                currPeakArray.push(currNum);
-            }
-            if (currNum === prevNum) {
-                currPeakArray = [currNum]
-            }
-        }
-    }
-    console.log("hasPeaked? end", hasPeaked);
-    if (currPeakArray.length > longestPeakArray.length && hasPeaked) {
-        longestPeakArray = currPeakArray;
-    }
-    return longestPeakArray.length;
-}
-
-// Do not edit the line below.
-exports.longestPeak = longestPeak;
+  // Do not edit the line below.
+  exports.longestPeak = longestPeak;
