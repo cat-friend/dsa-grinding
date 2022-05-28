b9df657400a424f6dbaebc2f5b5d76e774aa9707
cat-friend <denisepli@gmail.com>
2022-05-28 15:54:06 -0700

    2-shot merged-interval

--- algoexpert/medium/merge-overlapping-intervals.js
+++ algoexpert/medium/merge-overlapping-intervals.js
@@ -1,7 +1,22 @@
 function mergeOverlappingIntervals(array) {
-    // Write your code here.
-    return [[]];
-  }
+    array.sort((a, b) => {
+        return a[0] === b[0] ? (a[1] < b[1] ? -1 : 1) : a[0] - b[0];
+    })
+    console.log(array)
+    const mergedArray = [array.shift()];
+    console.log("mergedArray", mergedArray)
+    while (array.length) {
+        const currInterval = array.shift();
+        let lastValueInMergedArray = mergedArray[mergedArray.length - 1]
+        if (lastValueInMergedArray[1] >= currInterval[0]) {
+            if (lastValueInMergedArray[1] <= currInterval[1]) {
+                mergedArray[mergedArray.length - 1] = [lastValueInMergedArray[0], currInterval[1]];
+            }
+        }
+        else mergedArray.push(currInterval);
+    }
+    return mergedArray;
+}
 
-  // Do not edit the line below.
-  exports.mergeOverlappingIntervals = mergeOverlappingIntervals;
+// Do not edit the line below.
+exports.mergeOverlappingIntervals = mergeOverlappingIntervals;
