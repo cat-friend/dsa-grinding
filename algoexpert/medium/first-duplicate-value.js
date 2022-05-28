f102349af738fe48fcda8e3abe1ec7af2118bd3a
cat-friend <denisepli@gmail.com>
2022-05-28 15:07:31 -0700

    completed; 1 shot

--- algoexpert/medium/first-duplicate-value.js
+++ algoexpert/medium/first-duplicate-value.js
@@ -1,7 +1,17 @@
 function firstDuplicateValue(array) {
-    // Write your code here.
-    return [];
+    const uniqueNums = {}
+    for (const num of array) {
+        if (uniqueNums.hasOwnProperty(num)) {
+            return num;
+        }
+        else {
+            uniqueNums[num] = num;
+        }
+    }
+    return -1
   }
+
+  firstDuplicateValue([1, 2, 3, 4, 5, 6, 7, 7])
 
   // Do not edit the line below.
   exports.firstDuplicateValue = firstDuplicateValue;
