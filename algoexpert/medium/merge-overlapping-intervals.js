505bdeff2004cf7750b398ca2fe80d6d7b63e1da
cat-friend <denisepli@gmail.com>
2022-05-28 15:12:42 -0700

    new problem:  merge overlapping intervals

---
+++ algoexpert/medium/merge-overlapping-intervals.js
@@ -0,0 +1,7 @@
+function mergeOverlappingIntervals(array) {
+    // Write your code here.
+    return [[]];
+  }
+
+  // Do not edit the line below.
+  exports.mergeOverlappingIntervals = mergeOverlappingIntervals;
