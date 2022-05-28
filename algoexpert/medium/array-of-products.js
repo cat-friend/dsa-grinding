8866a0c5170b1815949fc6c61564d92fe8eb97a8
cat-friend <denisepli@gmail.com>
2022-05-28 12:46:13 -0700

    completed but in sub-optimal O(n)

--- algoexpert/medium/array-of-products.js
+++ algoexpert/medium/array-of-products.js
@@ -1,6 +1,18 @@
 function arrayOfProducts(array) {
-    // Write your code here.
-  }
+    const productArray = [];
 
-  // Do not edit the line below.
-  exports.arrayOfProducts = arrayOfProducts;
+    for (let i = 0; i < array.length; i++) {
+        let product = 1;
+        for (let j = 0; j < array.length; j++) {
+            if (j === i) { continue }
+            else {
+                product *= array[j];
+            }
+        }
+        productArray.push(product);
+    }
+    return productArray
+}
+
+// Do not edit the line below.
+exports.arrayOfProducts = arrayOfProducts;
