ab50d0370922464e060dd666c55ec3b953be8cc9
cat-friend <denisepli@gmail.com>
2022-06-02 15:00:37 -0700

    fleshed out logic of task assignment, need to refactor so it works

--- algoexpert/medium/task-assignment.js
+++ algoexpert/medium/task-assignment.js
@@ -5,15 +5,30 @@
  *
  * @returns returns array of pairs of indices of tasks assigned to each worker
  *  --> constraints task must be completed as fast as possible
- * Math.max, math.min - repetitive splicing
- * --> then key into object and
+ * split this into two functions:  sorted tasks, task index locator
  */
 
 function taskAssignment(k, tasks) {
-    const taskAssignmentArray = new Array.length(k).fill([]);
-    
-    return [];
+    const tasksCopy = [...tasks]
+    const taskAssignmentArray = new Array(k).fill([]);
+    let taskAssignmentArrayIndex = 0;
+    console.log("taskAA", taskAssignmentArray);
+    while (tasksCopy.length) {
+        const max = Math.max(...tasksCopy);
+        const maxIndex = tasksCopy.indexOf(max);
+        taskAssignmentArray[taskAssignmentArrayIndex].push(max);
+        tasksCopy.splice(maxIndex, 1);
+        const min = Math.min(...tasksCopy);
+        const minIndex = tasksCopy.indexOf(min);
+        taskAssignmentArray[taskAssignmentArrayIndex].push(min);
+        tasksCopy.splice(minIndex, 1);
+        taskAssignmentArray[taskAssignmentArrayIndex] = [maxIndex, minIndex]
+        taskAssignmentArrayIndex++;
+        console.log("tasksCopy", tasksCopy)
+    }
+    return taskAssignmentArray;
   }
 
+  console.log(taskAssignment(3, [1, 3, 5, 3, 1, 4]))
   // Do not edit the line below.
   exports.taskAssignment = taskAssignment;
