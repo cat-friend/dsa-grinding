c4b963e53536486bb2e5f21addaa324ca4bfdb77
cat-friend <denisepli@gmail.com>
2022-06-02 14:34:30 -0700

    new problem - task assignment

---
+++ algoexpert/medium/task-assignment.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {*} k - number of workers
+ * @param {*} tasks - array of positive integers representing durations of tasks that must be completed by the workers
+ *
+ * @returns returns array of pairs of indices of tasks assigned to each worker
+ *  --> constraints task must be completed as fast as possible
+ * Math.max, math.min - repetitive splicing
+ * --> then key into object and
+ */
+
+function taskAssignment(k, tasks) {
+    const taskAssignmentArray = new Array.length(k).fill([]);
+    
+    return [];
+  }
+
+  // Do not edit the line below.
+  exports.taskAssignment = taskAssignment;
