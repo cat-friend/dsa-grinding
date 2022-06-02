1ff23528c49dc9dd0887a179ad3f768ef5d4384d
cat-friend <denisepli@gmail.com>
2022-06-02 16:30:09 -0700

    new problem:  invert-binary-tree

---
+++ algoexpert/medium/invert-binary-tree.js
@@ -0,0 +1,17 @@
+// prob do bft
+
+function invertBinaryTree(tree) {
+    // Write your code here.
+  }
+
+  // This is the class of the input binary tree.
+  class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  // Do not edit the line below.
+  exports.invertBinaryTree = invertBinaryTree;
