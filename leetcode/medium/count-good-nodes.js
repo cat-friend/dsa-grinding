e1e6028ebcb3c7e1f4d6a4c2b0a78702ddbfb620
cat-friend <denisepli@gmail.com>
2022-06-08 17:06:08 -0700

    need to flesh out business logic more

--- leetcode/medium/count-good-nodes.js
+++ leetcode/medium/count-good-nodes.js
@@ -12,5 +12,22 @@
  */
  var goodNodes = function(root) {
      // dft --> use a stack
-
+     // need to keep track of max number during traversal
+     // root node is always good
+     // going to push and pop the end, evaluate against the max,
+     // need to keep track of number of good nodes
+     let goodCount = 0;
+     let max = root.val;
+     const stack = [];
+     stack.push(root)
+     while (stack.length) {
+         const currNode = stack.pop();
+         if (currNode.val >= max) {
+             goodCount++;
+             max = currNode.val;
+         }
+        if (currNode.left) stack.push(currNode.left);
+        if (currNode.right) stack.push(currNode.right);
+     }
+     return goodCount;
 };
