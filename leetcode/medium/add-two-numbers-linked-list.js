27d48ec3c545e065a810ab2d9e057e1d4597d881
cat-friend <denisepli@gmail.com>
2022-06-10 13:09:35 -0700

    need to generate LL

--- leetcode/medium/add-two-numbers-linked-list.js
+++ leetcode/medium/add-two-numbers-linked-list.js
@@ -10,15 +10,33 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-// spread operator the first one
-// iterate through second, add to same index on the first
-// !! watch out for l1[i] + l2[i] >= 10;
-    // l1[i+1] + l2[i+1] + Math.ceil((l1[i] + l2[i])/10)
-var addTwoNumbers = function (l1, l2) {
-    const output = [...l1];
-    output[0] = (l1[0] + l2[0]) % 10;
-    for (let i = 1; i < l2.length; i++) {
-        output[i] = l1[i] + l2[i] + Math.ceil((l1[i-1] + l2[i-1])/10)
+
+// make a stack! stack holds Math.floor((l1.val + l2.val)/10)
+// (l1.val + l2.val + stack.pop()) % 10 gets pushed to stack, added to the last value of the stack
+    // only need to use end of stack if stack.length - 1 > i;
+// if Math.floor((l1.val + l2.val)/10) > 0, then push Math.floor((l1.val + l2.val)/10) to stack
+// then return stack!
+ var addTwoNumbers = function(l1, l2) {
+    // first fill output array with l1's values
+    const output = [];
+    const stack = [0];
+    let currNode = l1;
+    while (currNode) {
+        output.push(currNode.val);
+        currNode = currNode.next;
+    };
+    // second, traverse l2
+        // output[i] = (l2.val + output[i] + stack.pop()) % 10;
+        // stack.push(Math.floor((l1.val + l2.val)/10)) --> will send a >= 0
+    currNode = l2;
+    let i = 0;
+    while (currNode) {
+        output[i] = output[i] ? (currNode.val + output[i] + stack.pop()) % 10 : (currNode.val + stack.pop()) % 10;
+        stack.push(Math.floor(currNode.val));
+        i++;
+        currNode = currNode.next
     }
+    if (stack[0] > 0) output.push(stack.pop());
+    // return output
     return output;
 };
