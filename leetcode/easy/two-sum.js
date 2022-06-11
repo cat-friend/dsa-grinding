2779cfe610a2754b390e372596bd21f138bb6f0e
denise <denisepli@gmail.com>
2022-06-11 15:30:01 -0700

    new problem: two-sum (indices); finished psuedocoding

---
+++ leetcode/easy/two-sum.js
@@ -0,0 +1,21 @@
+/**
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ */
+
+// make obj; value: [index];
+// iterate through nums array;
+// at each ele, target - nums = some difference
+// then check -- numsObj.hasOwnProperty(some difference) ? results.push(numsObj[some difference].pop() && results.push(numsObj[ele].pop()) && return results
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+
+};
