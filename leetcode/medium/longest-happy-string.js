60690ed5d3b8028ff4ad4a58c58ac6ef4bc3de42
denise <denisepli@gmail.com>
2022-06-13 15:48:58 -0700

    finished:  faster than 40% and less memory than 46%

--- leetcode/medium/longest-happy-string.js
+++ leetcode/medium/longest-happy-string.js
@@ -21,25 +21,42 @@ A substring is a contiguous sequence of characters within a string.
 // approach:
 // make an array of letters `happyString`
 // push letters into the array, when you push it in, subtract from argument
-// keep pushing in letters while the last 2 letters aren't that letter
+// keep pushing in letters while the last 2 letters aren't that letter and countLetter < a;
 // if last item in stack is not letter with count of 2, reset count to 0, push the letter, then add 1 to count, subtract from total count
+
+// this is a greedy algorithm problem -- find the letter with the most possible occurrences AT THE START OF EACH LOOP
+// mostLetter = goes first
+// push most letter into array
+// once second-most common letter is 0, we're done with the algorithm
 
 
 var longestDiverseString = function (a, b, c) {
+    const totalLength = a + b + c;
     const happyString = [];
-    while (a > 0 || b > 0 || c > 0) {
-        while (happyString[happyString.length - 1] != "a" && happyString[happyString.length - 2] != "a" && a > 0) {
-            happyString.push("a");
-            a--;
-        }
-        while (happyString[happyString.length - 1] != "b" && happyString[happyString.length - 2] != "b" && b > 0) {
-            happyString.push("b");
-            b--;
-        }
-        while (happyString[happyString.length - 1] != "c" && happyString[happyString.length - 2] != "c" && c > 0) {
-            happyString.push("c");
-            c--;
-        }
+    const chars = [
+        [a, 'a'],
+        [b, 'b'],
+        [c, 'c']
+    ]
+    while (happyString.length < totalLength) {
+        chars.sort((a, b) => b[0] - a[0]);
+        console.log("chars", chars);
+
+        // figure out which character to push
+        // if character at last item in stack and character at penultimate item in st ack are the same AND
+        // if character at last item is the nsame as GREEDIEST CHARACTER
+        // --> determines if the last two are the greediest character
+        // YES:  if last 2 in stack are greediest character, then character to push is the character in the second position
+        // NO:  if last 2 in the stack are NOT the greediest character, then the character to push is the greediest
+        const char = happyString[happyString.length - 1] === happyString[happyString.length - 2] && happyString[happyString.length - 1] === chars[0][1] ?
+        chars[1] : chars[0];
+        if (char[0] === 0) break;
+        happyString.push(char[1]);
+        // then deprecate the character that is pushed
+        char[0]--
+            // this deprecates the value in the chars array because they're pointing to the same things in memory -- the incoming a, b, c param
+        console.log("hppystringarray", happyString)
+        console.log("chars after push", chars)
     }
     return happyString.join("")
 };
