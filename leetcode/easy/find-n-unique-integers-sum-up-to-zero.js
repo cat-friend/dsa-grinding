c514ddee6781a2ce9e488fb2ec65905af6c3e4c0
cat-friend <denisepli@gmail.com>
2022-06-14 11:37:03 -0700

    logic for odd works, need to figure out logic for even

--- leetcode/easy/find-n-unique-integers-sum-up-to-zero.js
+++ leetcode/easy/find-n-unique-integers-sum-up-to-zero.js
@@ -2,6 +2,36 @@
  * @param {number} n
  * @return {number[]}
  */
- var sumZero = function(n) {
 
+/*
+
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+approach:
+make a new array with length `n`
+fill it with 0's
+left pointer and right pointer
+left pointer = math.floor(n/2);
+right pointer = n % 2 ? leftpointer + 2 : leftpointer + 1;
+*/
+var sumZero = function (n) {
+    const result = new Array(n).fill(0);
+    if (n = 1) return result;
+    let leftPointer = Math.floor(n / 2) - 1;
+    let rightPointer = n % 2 ? leftPointer + 2 : leftPointer + 1;
+    console.log("leftpointer at start", leftPointer, "\n", "rightPointer at start", rightPointer);
+    if (n % 2) {
+        while (leftPointer >= 0 && rightPointer < result.length) {
+            result[leftPointer] = result[leftPointer + 1] - 1;
+            result[rightPointer] = result[rightPointer - 1] + 1;
+            leftPointer--;
+            rightPointer++;
+        }
+    }
+    else {
+        while (leftPointer >= 0 )
+    }
+    return result;
 };
+
+console.log(sumZero(4));
