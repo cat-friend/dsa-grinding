63ba547330a7873a975defa9986242c8c9ce681d
denise <denisepli@gmail.com>
2022-06-19 19:24:16 -0700

    added some notes for approach

--- leetcode/easy/find-tic-tac-toe-winner.js
+++ leetcode/easy/find-tic-tac-toe-winner.js
@@ -2,6 +2,20 @@
  * @param {number[][]} moves
  * @return {string}
  */
+
+/*
+Instructions:
+return winner of game is winner exists
+if draw, return "Draw"
+if  there are still movements to play, return "Pending"
+APPROACH:
+- identify what constitutes a winner
+- separate moves into A vs B
+- see if either player wins
+- determine if there are still moves to play (no winner and < 9 moves)
+
+probably turn moves of A and moves of B into their own object, compare against properties of the win conditions
+*/
  var tictactoe = function(moves) {
 
 };
