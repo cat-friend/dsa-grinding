a9d7e8fecc66bb5c745c40c6cc4541d412aee325
denise <denisepli@gmail.com>
2022-06-22 09:24:00 -0700

    completed - used an object to store the indices that i've been to and then a counter to see how many times i've jumped around the array

--- algoexpert/medium/single-cycle-check.js
+++ algoexpert/medium/single-cycle-check.js
@@ -1,6 +1,30 @@
 function hasSingleCycle(array) {
-    // Write your code here.
-  }
+    // APPROACH:
+    // probably use an obj to keep track of visited indices
+    // use a while loop and have a counter
+    // when the counter = array.length, exit the while loop and return Boolean(obj.entries.length === array.length);
+    // if the obj contains the index, return true
+    // how to jump around the array?
+    // indexSum = currIndex + array[currIndex]
+    // if indexSum < 0, array.length + indexSum
+    let currIndex = 0;
+    let counter = 0;
+    const visited = {};
+    while (counter < array.length) {
+        const value = array[currIndex];
+        currIndex = currIndex + array[currIndex] < 0 ? array.length + ( (currIndex + array[currIndex]) % array.length) : (currIndex + array[currIndex]) % array.length;
+        console.log("currIndex", currIndex)
+        if (visited.hasOwnProperty(currIndex)) {
+            if (counter === array.length - 1 && Boolean(Object.entries(visited).length === array.length)) return true;
+            return false
+        };
+        visited[currIndex] = currIndex;
+        counter++
+        console.log("counter", counter, "\nvisited", visited)
+    }
+    return Boolean(Object.entries(visited).length === array.length);
+}
 
-  // Do not edit the line below.
-  exports.hasSingleCycle = hasSingleCycle;
+// console.log(hasSingleCycle([2, 3, 1, -4, -4, 2]))
+// Do not edit the line below.
+exports.hasSingleCycle = hasSingleCycle;
