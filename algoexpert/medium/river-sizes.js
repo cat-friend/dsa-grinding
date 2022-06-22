fda573c4872058d04a8941497089e26f0eacd2ed
denise <denisepli@gmail.com>
2022-06-22 10:06:54 -0700

    approach done-ish, breaking to do system design

---
+++ algoexpert/medium/river-sizes.js
@@ -0,0 +1,37 @@
+function riverSizes(matrix) {
+    // APPROACH:
+    // depth first search -
+    // need a set to keep track of nodes visited
+    // first need to find a hit - iterate through array until you find a hit
+    // when you find a hit, check NWES for adjacent hits, if there are adjacent hits, check if it's in the set, if it isn't, add it to the queue of hits --> key into subsequent hits and repeat
+    // need a `while` loop for the queue
+    // when the queue is empty, push the river size to the river length array
+    // IS THERE A WAY TO DO IT WITHOUT TWO NESTED `for` LOOPS?
+    const sizes = [];
+    for (let i = 0; i < matrix.length; i ++) {
+        for (let j = 0; j < matrix[i].length; i++) {
+            
+        }
+    }
+    return sizes
+}
+
+function findHits(matrix, coordinates, set) {
+    // coordinates: [i][j]
+    const hits = [];
+    // find north
+    // if it's at the top (i = 0), can't find north, otherwise, [i - 1][j]
+
+    // find south
+    // if it's at the bottom (i = matrix.length - 1), can't find south, otherwise [i + 1]
+
+    // find east
+    // if it's at [i][matrix[i].length - 1], can't find east; otherwise [i][j+1]
+
+    // find west
+    // if it's at [i][0] can't find west, otherwise [i][j - 1]
+    return hits
+}
+
+// Do not edit the line below.
+exports.riverSizes = riverSizes;
