7f1457b0a09a46c04df0244e3ca4834e71a894aa
denise <denisepli@gmail.com>
2022-06-23 10:35:33 -0700

    completed!

--- algoexpert/medium/river-sizes.js
+++ algoexpert/medium/river-sizes.js
@@ -2,80 +2,88 @@ function riverSizes(matrix) {
     // APPROACH:
     // depth first search -
     // need a set to keep track of nodes visited
-    // first need to find a hit - iterate through array until you find a hit
-    // when you find a hit, check NWES for adjacent hits, if there are adjacent hits, check if it's in the set, if it isn't, add it to the queue of hits --> key into subsequent hits and repeat
-    // need a `while` loop for the queue
     // when the queue is empty, push the river size to the river length array
     // IS THERE A WAY TO DO IT WITHOUT TWO NESTED `for` LOOPS?
     const sizes = [];
     const visitedNodes = new Set();
-    let riverSize = [];
+
+    // first need to find a hit - iterate through array until you find a hit
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] === 1 && !visitedNodes.has(`[${i}, ${j}]`)) {
-                riverSize.push([i, j]);
-                visitedNodes.add(`${[i, j]}`)
-                console.log("visitedNodes", visitedNodes)
-                let adjacentNodes = findAdjacentHits(matrix, [i, j], visitedNodes);
-                while (adjacentNodes.length) {
-                    const currNode = adjacentNodes.pop();
-                    riverSize.push(currNode)
-                    adjacentNodes = adjacentNodes.concat(findAdjacentHits(matrix, currNode, visitedNodes));
-                }
-                console.log("riverSize", riverSize);
-                if (riverSize.length) sizes.push(riverSize.length);
-                riverSize = [];
+            // when you find a hit, check if it's in the visistedNodes set
+            // if yes, continue
+            if (matrix[i][j] === 1 && !visitedNodes.has(`${i}, ${j}`)) {
+                // if not, add it to the set
+                visitedNodes.add(`${i}, ${j}`)
+                // send the hit to the riverLength function
+                const riverLength = findRiverLength(matrix, [i, j], visitedNodes);
+                if (riverLength) sizes.push(riverLength);
             }
         }
     }
     return sizes
 }
 
-function findAdjacentHits(matrix, coordinates, set) {
-    const [i, j] = coordinates;
-    const hits = [];
+// pushes coordinates to riverLengthArray
+// has a queue of adjacent nodes
+// keeps looking for adjacent nodes of adjacent nodes until queue is empty
+// --> need a `while` loop for the queue
+// --> check NWES of the initial hit for adjacent hits
+// if adjacent node is 1 and is not in the set, push to adjacent node queue and add to riverNodes array
+// return riverNodes.length;
+
+// riverLength function - takes in matrix, coordinates, set
+function findRiverLength(matrix, coordinates, set) {
+    const riverNodes = [coordinates];
+    const adjacentNodes = [coordinates];
     // find north
     // if it's at the top (i = 0), can't find north, otherwise, [i - 1][j]
-    if (i > 0) {
-        if (matrix[i - 1][j]) {
-            if (!set.has(`${[i - 1, j]}`)) {
-                hits.push([i - 1, j]);
-                set.add(`${[i - 1, j]}`)
+    while (adjacentNodes.length) {
+        const [i, j] = adjacentNodes.pop();
+        if (i > 0) {
+            if (matrix[i - 1][j]) {
+                if (!set.has(`${i - 1}, ${j}`)) {
+                    riverNodes.push([i - 1, j]);
+                    adjacentNodes.push([i - 1, j]);
+                    set.add(`${i - 1}, ${j}`)
+                }
+            }
+        }
+        // find south
+        // if it's at the bottom (i = matrix.length - 1), can't find south, otherwise [i + 1]
+        if (i < matrix.length - 1) {
+            if (matrix[i + 1][j]) {
+                if (!set.has(`${i + 1}, ${j}`)) {
+                    riverNodes.push([i + 1, j]);
+                    adjacentNodes.push([i + 1, j]);
+                    set.add(`${i + 1}, ${j}`)
+                }
+            }
+        }
+        // find east
+        // if it's at [i][matrix[i].length - 1], can't find east; otherwise [i][j+1]
+        if (j < matrix[i].length - 1) {
+            if (matrix[i][j + 1]) {
+                if (!set.has(`${i}, ${j + 1}`)) {
+                    riverNodes.push([i, j + 1]);
+                    adjacentNodes.push([i, j + 1]);
+                    set.add(`${i}, ${j + 1}`);
+                }
+            }
+        }
+        // find west
+        // if it's at [i][0] can't find west, otherwise [i][j - 1]
+        if (j > 0) {
+            if (matrix[i][j - 1]) {
+                if (!set.has(`${i}, ${j - 1}`)) {
+                    riverNodes.push([i, j - 1]);
+                    adjacentNodes.push([i, j - 1]);
+                    set.add(`${i}, ${j - 1}`);
+                }
             }
         }
     }
-    // find south
-    // if it's at the bottom (i = matrix.length - 1), can't find south, otherwise [i + 1]
-    if (i < matrix.length - 1) {
-        if (matrix[i + 1][j]) {
-            if (!set.has(`${[i + 1, j]}`)) {
-                hits.push([i + 1, j]);
-                set.add(`${[i + 1, j]}`)
-            }
-        }
-    }
-    // find east
-    // if it's at [i][matrix[i].length - 1], can't find east; otherwise [i][j+1]
-    if (j < matrix[i].length - 1) {
-        if (matrix[i][j + 1]) {
-            if (!set.has(`${[i, j + 1]}`)) {
-                hits.push([i, j + 1]);
-                set.add(`${[i, j + 1]}`);
-            }
-        }
-    }
-    // find west
-    // if it's at [i][0] can't find west, otherwise [i][j - 1]
-    if (j > 0) {
-        if (matrix[i][j - 1]) {
-            if (!set.has(`${[i, j - 1]}`)) {
-                hits.push([i, j - 1]);
-                set.add(`${[i, j - 1]}`);
-            }
-        }
-    }
-    console.log("hits", hits)
-    return hits
+    return riverNodes.length
 }
 
 console.log(riverSizes([
