097ddc721ca6ddf5ea83bd21e8a7bee5f3849b17
denise <denisepli@gmail.com>
2022-06-26 20:19:40 -0700

    BRAIN TIRED

---
+++ leetcode/microsoft/minimum-deletions-to-make-sttring-balanced.js
@@ -0,0 +1,21 @@
+/*
+You are given a string s consisting only of characters 'a' and 'b'​​​​.
+
+You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+
+Return the minimum number of deletions needed to make s balanced.
+
+APPROACH:
+find where the first b is, find where the last a is
+count how many b's between the first b and the last a
+count how many a's between the first b and the last a
+*/
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var minimumDeletions = function(s) {
+
+};
