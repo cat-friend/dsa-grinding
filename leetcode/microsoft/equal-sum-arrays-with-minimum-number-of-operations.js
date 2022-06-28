5fa9f9de72c3046f57c9299c05f92166d9c9b7b8
cat-friend <denisepli@gmail.com>
2022-06-28 13:43:05 -0700

    completed using dynamic programming

--- leetcode/microsoft/equal-sum-arrays-with-minimum-number-of-operations.js
+++ leetcode/microsoft/equal-sum-arrays-with-minimum-number-of-operations.js
@@ -34,7 +34,30 @@ Constraints:
 
 1 <= nums1.length, nums2.length <= 105
 1 <= nums1[i], nums2[i] <= 6
+
+APPROACH
+seems like a greedy algorithm problem, so probably will need to find max values and their indices
+
+first find the sums of each of the arrays
+then find the difference between them
+method:
+    find the lesser sum array:
+        find the min value and max it out
+        then find the diff between max value (6) and the value it is and push it to a range array
+        so rangeArray.push(6 - lesser[i])
+    for the great sum array:
+        find the max value and min it out
+        find the diff between min value (1) and the max value and push it to the range array
+        so rangeArray.push(greater[i] - 1)
+    then a `for` loop for range array
+    need to do diff -= rangearray[i] and permutations++
+    if diff <= 0 break;
+    return diff <= 0 ? permutations : -1;
+
+PROBABLY NEED TO SORT RANGE ARRAY b - a because the larger values will give the biggest delta in diff when it is -=
+
 */
+
 
 
 /**
@@ -42,6 +65,38 @@ Constraints:
  * @param {number[]} nums2
  * @return {number}
  */
- var minOperations = function(nums1, nums2) {
+var minOperations = function (nums1, nums2) {
+    let sum1 = 0;
+    let sum2 = 0;
+    let lesser;
+    let greater;
+    let difference;
+    let permutations = 0;
+    const rangeArray = [];
+    for (const num of nums1) {
+        sum1 += num;
+    }
+    for (const num of nums2) {
+        sum2 += num;
+    }
 
+    difference = Math.abs(sum1 - sum2);
+    lesser = sum1 < sum2 ? nums1 : nums2;
+    greater = sum1 > sum2 ? nums1 : nums2;
+    for (let i = 0; i < lesser.length; i++) {
+        rangeArray.push(6 - lesser[i]);
+    }
+    for (let i = 0; i < greater.length; i++) {
+        rangeArray.push(greater[i] - 1);
+    }
+
+    rangeArray.sort((a, b) => b - a);
+    for (const num of rangeArray) {
+        if (difference <= 0) break;
+        difference -= num;
+        permutations++;
+    }
+    return difference <= 0 ? permutations : -1;
 };
+
+console.log(minOperations([1,2,3,4,5,6], [1,1,2,2,2,2]));
