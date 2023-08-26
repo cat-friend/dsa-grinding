be6258fc868fa759e7c48298798c9ac309bb1faf
cat-friend <denisepli@gmail.com>
2023-08-26 09:43:05 -0700

    171 first attempt done

--- leetcode/easy/171-excel-sheet-column-number.js
+++ leetcode/easy/171-excel-sheet-column-number.js
@@ -39,5 +39,51 @@ columnTitle is in the range ["A", "FXSHRXW"].
  * @return {number}
  */
 var titleToNumber = function(columnTitle) {
-
+    const alphabet = {
+        A: 1,
+        B: 2,
+        C: 3,
+        D: 4,
+        E: 5,
+        F: 6,
+        G: 7,
+        H: 8,
+        I: 9,
+        J: 10,
+        K: 11,
+        L: 12,
+        M: 13,
+        N: 14,
+        O: 15,
+        P: 16,
+        Q: 17,
+        R: 18,
+        S: 19,
+        T: 20,
+        U: 21,
+        V: 22,
+        W: 23,
+        X: 24,
+        Y: 25,
+        Z: 26,
+    }
+    let colNum = 0;
+    let counter = 0;
+    let index = columnTitle.length - 1;
+    while (counter < columnTitle.length) {
+        colNum = alphabet[columnTitle[index]] * Math.pow(26, counter) + colNum;
+        counter++
+        index--
+    }
+    return colNum;
 };
+
+console.log(titleToNumber("ZY"))
+
+/**
+ * Results:
+ *
+ * 1st attempt:
+ * Runtime: 60 ms (75%)
+ * Memory: 44.70 mb (5.37%)
+ */
